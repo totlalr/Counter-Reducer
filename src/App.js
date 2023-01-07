b24b8ccee3a1d5ccb9c0e47b277e8cf1f2836466
Armin Vaghef <armin88vaghef@gmail.com>
2023-01-07 15:19:33 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
 
+const initialState = 0;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREAMENT":
+      return state + 1;
+    case "DECREAMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
 function App() {
+  const [number, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => dispatch({ type: "INCREAMENT" })}>
+        Increament
+      </button>
+      <br />
+      <br />
+      <button onClick={() => dispatch({ type: "DECREAMENT" })}>
+        Decreament
+      </button>
+
+      <hr />
+      {number}
     </div>
   );
 }
